97280f5a8596400338d3e377fb765757996b8f9d
Francois Polo <andasan@gmail.com>
2020-07-08 12:15:50 -0700

    delete data with mutation

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { GraphQLServer } from 'graphql-yoga';
 import { v4 as uuidv4} from 'uuid';
 
 //dummy data
-const users = [
+let users = [
     {
         id: '1',
         name: 'Potato',
@@ -17,7 +17,7 @@ const users = [
     }
 ];
 
-const posts = [
+let posts = [
     {
         id: '10',
         title: 'GraphQL Basics',
@@ -41,7 +41,7 @@ const posts = [
     }
 ];
 
-const comments = [
+let comments = [
     {
         id: '1',
         text: 'Yummy',
@@ -66,7 +66,31 @@ const typeDefs = `
     }
 
     type Mutation {
-        createUser(name: String!, email: String!, age: Int) : User!
+        createUser(data: CreateUserInput) : User!
+        createPost(data: CreatePostInput) : Post!
+        createComment(data: CreateCommentInput) : Comment!
+        deleteUser(id: ID!): User!
+        deletePost(id: ID!): Post!
+        deleteComment(id: ID!): Comment!
+    }
+
+    input CreateUserInput {
+        name: String!
+        email: String!
+        age: Int
+    }
+
+    input CreatePostInput{
+        title: String!
+        body: String!
+        published: Boolean!
+        author: ID!
+    }
+
+    input CreateCommentInput{
+        text: String!
+        author: ID!
+        post: ID!
     }
 
     type User {
@@ -135,7 +159,7 @@ const resolvers = {
     Mutation: {
         createUser(parent, args,ctx,info){
             // console.log(args);
-            const emailTaken = users.some(user => user.email === args.email)
+            const emailTaken = users.some(user => user.email === args.data.email)
 
             if(emailTaken){
                 throw new Error('Email is already taken');
@@ -143,14 +167,72 @@ const resolvers = {
 
             const user = {
                 id: uuidv4(),
-                name: args.name,
-                email: args.email,
-                age: args.age
+                ...args.data
             }
 
             users.push(user);
 
             return user;
+        },
+
+        createPost(parent, args,ctx,info){
+            const userExist = users.some(user => user.id === args.data.author);
+
+            if(!userExist){
+                throw new Error('User not found');
+            }
+
+            const post = {
+                id:  uuidv4(),
+                ...args.data
+            }
+
+            posts.push(post);
+
+            return post;
+        },
+
+        createComment(parent, args,ctx,info){
+            const userExists = users.some(user => user.id === args.data.author);
+            const postExists = posts.some(post => post.id === args.data.post && post.published);
+
+            if(!userExists || !postExists){
+                throw new Error('Unable to find user and post');
+            }
+
+            const comment = {
+                id: uuidv4(),
+                ...args.data
+            }
+
+            comments.push(comment);
+
+            return comment
+        },
+
+        deleteUser(parent,args,ctx,info){
+            const userIndex = users.findIndex(user => user.id === args.id);
+
+            if(userIndex === -1){
+                throw new Error('User not found');
+            }
+
+            const deletedUsers = users.splice(userIndex,1);
+
+            posts = posts.filter(post => {
+                const match = post.author === args.id
+
+                if(match){
+                    comments = comments.filter(comment => comment.post !== post.id)
+                }
+
+                return !match;
+            });
+
+            comments = comment.filter(comment => comment.author !== args.id)
+
+            return deletedUsers[0];
+
         }
     },
     Post: {
